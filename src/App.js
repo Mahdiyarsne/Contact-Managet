6b29e3b01b4822f95213e3f6141cb1ea6e1739a0
Mahdiyar <mahdiyarsne98@gmail.com>
2025-09-22 14:01:07 +0330

    فونت و  پکیج های مورد نیاز نصب و پیکربندی شد

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div className='App'>
       <h3>مدیریت مخاطبین</h3>
+      <span className='fa fa-user'></span>
+      <button className='btn btn-primary'>کلیک کن</button>
     </div>
   );
 };
