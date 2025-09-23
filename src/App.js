00b7409de84c26269da2b4aed0b54fb2837b30c3
Mahdiyar <mahdiyarsne98@gmail.com>
2025-09-23 13:43:54 +0330

    کامپوننت مخاطبین و مخاطب اضافه شد

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 
 import './App.css';
+import Contacts from './components/contact/Contacts';
 
 const App = () => {
   return (
     <div className='App'>
       <Navbar />
+      <Contacts/>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
           </div>
           <div className='col'>
             <SearchContact />
+            
           </div>
         </div>
       </div>
